215d77e57c8657fa95ad7d24fa87d7a6b504505d
EddiePacheco <eddiepacheco@pm.me>
2025-01-02 17:17:44 -0800

    Improved regex search code legibility by using the glob() method.

--- Chapter_09_Reading_Writing_Files/RegexSearch.py
+++ Chapter_09_Reading_Writing_Files/RegexSearch.py
@@ -3,16 +3,24 @@
 
 import re
 import os
+from pathlib import Path
 
 # Prompt the user for a regular expression
 print('Enter a regular expression:\n')
 UserRegex = re.compile(input())
 
 # Open all .txt files in the current working directory
-for filename in os.listdir(os.getcwd()):
-    if filename.endswith('.txt'):
-        with open(filename, 'r') as File:
-            FileContent = File.readlines()
-            for line in FileContent:
-                if UserRegex.search(line):
-                    print(line)
+CurrDir = Path.cwd()
+for filename in CurrDir.glob('*.txt'):
+    with open(filename, 'r') as File:
+        FileContent = File.readlines()
+        for line in FileContent:
+            if UserRegex.search(line):
+                print(line)
+# for filename in os.listdir(os.getcwd()):
+#     if filename.endswith('.txt'):
+#         with open(filename, 'r') as File:
+#             FileContent = File.readlines()
+#             for line in FileContent:
+#                 if UserRegex.search(line):
+#                     print(line)
