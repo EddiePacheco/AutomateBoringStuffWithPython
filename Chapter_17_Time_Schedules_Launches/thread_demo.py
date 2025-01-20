f0caff843d6d4ad88dcaa51053b079677c35d9d0
Eddie Pacheco <34888786+EddiePacheco@users.noreply.github.com>
2025-01-19 19:57:04 -0800

    Datetime module and multithreading.

---
+++ Chapter_17_Time_Schedules_Launches/thread_demo.py
@@ -0,0 +1,13 @@
+#! python3
+import threading, time
+
+print('Start of program.')
+
+def take_a_nap():
+    time.sleep(5)
+    print('Wake up!')
+
+thread_obj = threading.Thread(target=take_a_nap)
+thread_obj.start()
+
+print('End of program.')
